64fd5502f2f2bc387f4022aae5dd3d6acec8c2c6
carban <murillo.carlos@correounivalle.edu.co>
2020-04-26 17:29:25 -0500

    final i guess

--- tvshow_costs/backend/index.js
+++ tvshow_costs/backend/index.js
@@ -22,6 +22,39 @@ const pathModel = "/home/carban/Documents/ConstraintProgramming/CPProject/please
 const pathModel2 = "/home/carban/Documents/ConstraintProgramming/CPProject/MOdelPart2.mzn";
 
 
+app.post("/api/fileProcess/", (req, res) => {
+    const { file } = req.body;
+    try {
+        exec("cat "+file, (err, stdout, stderr) => {
+            var s = stdout;
+            s = s.split("\n").join("");
+            s = s.trim();
+            s = s.split(";");
+
+            var vals = [];
+
+            for (let i = 0; i < s.length-1; i++) {
+                s[i] = s[i].trim();
+                var aux = s[i].split("=");
+                vals.push(aux[1]);
+            }
+
+            if (s.length-1 == 3) {
+                // MODEL1
+                res.json({model: 1, actores: vals[0], escenas: vals[1], duracion: vals[2]});
+            }else{
+                // MODEL2
+                res.json({model: 2, actores: vals[0], escenas: vals[1], duracion: vals[2], disponibilidad: vals[3], evitar: vals[4]});
+            }
+        });
+    } catch (error) {
+        res.json({ error: error });
+    }
+
+});
+
+
+// #################################### MODEL 1 ################################
 app.post('/api/solve/model1/', (req, res) => {
 
     const { actores, escenas, duracion } = req.body;
@@ -50,31 +83,6 @@ app.post('/api/solve/model1/', (req, res) => {
     }
 })
 
-app.post("/api/solve/model1/fileProcess/", (req, res) => {
-    const { file } = req.body;
-    try {
-        exec("cat "+file, (err, stdout, stderr) => {
-            var s = stdout;
-            s = s.split("\n").join("");
-            s = s.trim();
-            s = s.split(";");
-
-            var vals = [];
-
-            for (let i = 0; i < 3; i++) {
-                s[i] = s[i].trim();
-                var aux = s[i].split("=");
-                vals.push(aux[1]);
-            }
-            res.json({actores: vals[0], escenas: vals[1], duracion: vals[2]});
-
-        });
-    } catch (error) {
-        res.json({ error: error });
-    }
-
-});
-
 app.post('/api/solve/model1/file/', (req, res) => {
 
     const { file } = req.body;
@@ -100,3 +108,59 @@ app.post('/api/solve/model1/file/', (req, res) => {
         res.json({ error: error });
     }
 })
+
+
+// #################################### MODEL 2 ################################
+
+app.post('/api/solve/model2/', (req, res) => {
+
+    const { actores, escenas, duracion, disponibilidad, evitar } = req.body;
+
+    const inpp = ' -D \" ' + actores + ' ' + escenas + ' ' + duracion + ' ' + disponibilidad + ' ' + evitar + '"';
+
+    var command = '/..' + pathMini + ' --solver Chuffed ' + pathModel2 + inpp;
+
+    // console.log(command);
+
+    // DIRECTO
+    try {
+        exec(command, (err, stdout, stderr) => {
+            if (stdout.length != 24) {
+
+                var fS = stdout.split(";");
+                console.log({ sol: fS[0], costo: fS[1], tiempo: fS[2] });
+                res.json({ sol: fS[0], costo: fS[1], tiempo: fS[2] });
+            } else {
+                console.log('UNSATISFIABLE');
+                res.json({ msg: 'UNSATISFIABLE' });
+            }
+        });
+    } catch (error) {
+        res.json({ error: error });
+    }
+})
+
+app.post("/api/solve/model2/file/", (req, res) => {
+    const { file } = req.body;
+
+    var command = '/..' + pathMini + ' --solver Chuffed ' + pathModel2 + ' ' +file;
+
+    console.log(command);
+
+    // DIRECTO
+    try {
+        exec(command, (err, stdout, stderr) => {
+            if (stdout.length != 24) {
+
+                var fS = stdout.split(";");
+                console.log({ sol: fS[0], costo: fS[1], tiempo: fS[2] });
+                res.json({ sol: fS[0], costo: fS[1], tiempo: fS[2] });
+            } else {
+                console.log('UNSATISFIABLE');
+                res.json({ msg: 'UNSATISFIABLE' });
+            }
+        });
+    } catch (error) {
+        res.json({ error: error });
+    }
+});
